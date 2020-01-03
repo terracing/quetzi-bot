776c97ac31ceeb1ddc63e3ceb3eae446d48ca0e0
Omar Terrazas <terrazas.omar@gmail.com>
2020-01-02 21:39:29 -0600

    Quetzi section with BotUI interaction.

--- assets/js/quetzi.js
+++ assets/js/quetzi.js
@@ -2,17 +2,42 @@ var botui = new BotUI('my-botui-app');
 botui.message.add({
     content: '¡Hola! Soy Quetzi'
 }).then(function () { // wait till previous message has been shown.
-
     botui.message.add({
         delay: 1000,
-        human: true,
-        content: 'Hello World from human!'
+        loading: true,
+        content: 'Estoy aquí para ayudar a la comunidad docente a crear contenidos geniales.'
+    }).then(function () {
+        botui.message.add({
+            delay: 1000,
+            human: true,
+            content: '¿Cómo haces eso?'
+        }).then(function () {
+            botui.message.add({
+                delay: 1000,
+                loading: true,
+                type: 'html',
+                content: 'A través de <strong>bits</strong> de información práctica que les muestra como desarrollar lo que imaginan.'
+            }).then(function () {
+                botui.message.add({
+                    delay: 500,
+                    human: true,
+                    content: '¿Bits de información?'
+                }).then(function () {
+                    botui.message.add({
+                        delay: 1000,
+                        loading: true,
+                        type: 'html',
+                        content: 'Revisa la sección de <a href="#bits">bits</a> para que veas de qué hablo.'
+                    }).then(function () {
+                        botui.message.add({
+                            delay: 1000,
+                            loading: true,
+                            type: 'html',
+                            content: 'No olvides suscribirte al <a href="#noticias">boletín de noticias</a> para mantenerte al tanto de mis novedades.'
+                        });
+                    });
+                });
+            });
+        });
     });
-});
-botui.action.text({
-    action: {
-        placeholder: 'Enter your text here'
-    }
-}).then(function (res) { // will be called when it is submitted.
-    console.log(res.value); // will print whatever was typed in the field.
 });
