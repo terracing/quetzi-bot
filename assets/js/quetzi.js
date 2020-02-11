922f57417420a1be7edaa58d63446542943fff43
Omar Terrazas <terrazas.omar@gmail.com>
2020-02-11 00:10:55 -0600

    Firsts styles for desktop version.

--- assets/js/quetzi.js
+++ assets/js/quetzi.js
@@ -1,52 +1,7 @@
 $(function(){
-    $('.sidenav').sidenav();
-
+    $(window).scroll(function () {
+        console.log($(window).scrollTop())
+        $('img#back').css('right',-($(window).scrollTop()) + 'px')
+    })
     
-    $('.botui-message:nth-child(1)').fadeIn(1000, function(){
-        $('.botui-message:nth-child(2)').fadeIn(1000, function(){
-            setTimeout(function(){
-                $('.botui-message:nth-child(2)').fadeOut(500, function(){
-                    $('.botui-message:nth-child(3)').fadeIn(1000, function() {
-                        setTimeout(function(){
-                            $('.botui-message:nth-child(4)').fadeIn(1000, function(){
-                                setTimeout(function(){
-                                    $('.botui-message:nth-child(5)').fadeIn(1000, function(){
-                                        setTimeout(function(){
-                                            $('.botui-message:nth-child(5)').fadeOut(500, function(){
-                                                $('.botui-message:nth-child(6)').fadeIn(1000, function() {
-                                                    setTimeout(function(){
-                                                        $('.botui-message:nth-child(7)').fadeIn(1000, function(){
-                                                            setTimeout(function(){
-                                                                $('.botui-message:nth-child(8)').fadeIn(1000, function() {
-                                                                    setTimeout(function(){
-                                                                        $('.botui-message:nth-child(8)').fadeOut(500, function(){
-                                                                            $('.botui-message:nth-child(9)').fadeIn(1000, function() {
-                                                                                setTimeout(function(){
-                                                                                    $('.botui-message:nth-child(10)').fadeIn(1000, function(){
-                                                                                        setTimeout(function(){
-                                                                                            $('.botui-message:nth-child(10)').fadeOut(500, function(){
-                                                                                                $('.botui-message:nth-child(11)').fadeIn(1000);
-                                                                                            });     
-                                                                                        }, 1000);
-                                                                                    });
-                                                                                }, 1000);
-                                                                            });
-                                                                        });
-                                                                    }, 1000);
-                                                                });
-                                                            }, 1000);
-                                                        });
-                                                    }, 3000);
-                                                });
-                                            });
-                                        }, 1000);
-                                    });
-                                }, 1000);
-                            });
-                        }, 3000);
-                    });
-                });
-            }, 1000);
-        });
-    });
 });
