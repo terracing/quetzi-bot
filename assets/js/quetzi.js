0a9c5f78960ee2a865670c4f3cebd2ffb6423d4f
Omar Terrazas <terrazas.omar@gmail.com>
2020-02-12 00:12:39 -0600

    Animations.

--- assets/js/quetzi.js
+++ assets/js/quetzi.js
@@ -1,7 +1,29 @@
-$(function(){
+$(function () {
+    sizeBackgroundMain();
     $(window).scroll(function () {
-        console.log($(window).scrollTop())
-        $('img#back').css('right',-($(window).scrollTop()) + 'px')
+        $('img#back').css('right', -($(window).scrollTop()) + 'px')
+
+        const Info = $('#info').offset();
+        if ($(window).scrollTop() >= (Info.top - 150)) {
+            $('nav').addClass('no-transparent');
+        } else {
+            $('nav').removeClass('no-transparent')
+        }
+
+        const Contact = $('section#contact').offset();
+        if ($(window).scrollTop() >= (Contact.top - 50)) {
+            $('nav').addClass('contact-header');
+            $('nav').removeClass('no-transparent');
+        } else {
+            $('nav').removeClass('contact-header');
+        }
     })
-    
 });
+
+function sizeBackgroundMain() {
+    const backWidth = ($('#quetzi').offset().left * 1.2) + 'px';
+    $('#back').css({
+        'width': backWidth,
+        'top': -($('#quetzi').height() * 0.1)
+    });
+}
